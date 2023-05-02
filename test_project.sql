749c0d676a9fa928d7b5d1666e40b216a6b88c77
memisgunduzz <memisgunduz@rissasoft.com>
2023-05-02 20:53:28 +0300

    Updated database file in project files.

--- test_project.sql
+++ test_project.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Anamakine: localhost
--- Üretim Zamanı: 14 Oca 2023, 13:53:37
+-- Üretim Zamanı: 02 May 2023, 19:52:15
 -- Sunucu sürümü: 10.4.21-MariaDB
 -- PHP Sürümü: 7.4.28
 
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Veritabanı: `test_project`
+-- Veritabanı: `ocr`
 --
 
 -- --------------------------------------------------------
@@ -40,16 +40,30 @@ CREATE TABLE `failed_jobs` (
 -- --------------------------------------------------------
 
 --
--- Tablo için tablo yapısı `files`
+-- Tablo için tablo yapısı `get_data`
 --
 
-CREATE TABLE `files` (
+CREATE TABLE `get_data` (
+  `id` int(11) NOT NULL,
+  `json_data` text NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `jobs`
+--
+
+CREATE TABLE `jobs` (
   `id` bigint(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `file_path` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `file_to_text` longtext COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL
+  `queue` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
+  `attempts` tinyint(3) UNSIGNED NOT NULL,
+  `reserved_at` int(10) UNSIGNED DEFAULT NULL,
+  `available_at` int(10) UNSIGNED NOT NULL,
+  `created_at` int(10) UNSIGNED NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
@@ -73,7 +87,22 @@ INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
 (2, '2014_10_12_100000_create_password_resets_table', 1),
 (3, '2019_08_19_000000_create_failed_jobs_table', 1),
 (4, '2019_12_14_000001_create_personal_access_tokens_table', 1),
-(5, '2023_01_14_065806_create_files_table', 1);
+(5, '2023_03_13_110035_create_ocr_job_blocks_table', 1),
+(6, '2023_03_13_151640_create_jobs_table', 2),
+(7, '2023_04_27_151639_create_rules_table', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Tablo için tablo yapısı `ocr_job_blocks`
+--
+
+CREATE TABLE `ocr_job_blocks` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `block_id` int(11) NOT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 -- --------------------------------------------------------
 
@@ -109,6 +138,33 @@ CREATE TABLE `personal_access_tokens` (
 -- --------------------------------------------------------
 
 --
+-- Tablo için tablo yapısı `rules`
+--
+
+CREATE TABLE `rules` (
+  `id` bigint(20) UNSIGNED NOT NULL,
+  `rule` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `state` int(11) NOT NULL DEFAULT 0,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Tablo döküm verisi `rules`
+--
+
+INSERT INTO `rules` (`id`, `rule`, `state`, `created_at`, `updated_at`) VALUES
+(1, 'Hallo, dies ist ein Testbeitrag', 0, NULL, NULL),
+(2, 'Das ist ein Beispiel für Deutschland.', 0, NULL, NULL),
+(3, 'postman1', 0, NULL, NULL),
+(6, 'Yet more text.\r\n', 0, NULL, NULL),
+(8, 'Boring. More, a little more text. The end, and just as well.\n', 0, NULL, NULL),
+(9, 'And more text1', 0, NULL, NULL),
+(10, 'And more text. And more text. And more text. And more text.', 0, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Tablo için tablo yapısı `users`
 --
 
@@ -135,15 +191,28 @@ ALTER TABLE `failed_jobs`
   ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
 
 --
--- Tablo için indeksler `files`
+-- Tablo için indeksler `get_data`
 --
-ALTER TABLE `files`
+ALTER TABLE `get_data`
   ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `jobs`
+--
+ALTER TABLE `jobs`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `jobs_queue_index` (`queue`);
 
 --
 -- Tablo için indeksler `migrations`
 --
 ALTER TABLE `migrations`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Tablo için indeksler `ocr_job_blocks`
+--
+ALTER TABLE `ocr_job_blocks`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -161,6 +230,12 @@ ALTER TABLE `personal_access_tokens`
   ADD KEY `personal_access_tokens_tokenable_type_tokenable_id_index` (`tokenable_type`,`tokenable_id`);
 
 --
+-- Tablo için indeksler `rules`
+--
+ALTER TABLE `rules`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Tablo için indeksler `users`
 --
 ALTER TABLE `users`
@@ -175,25 +250,43 @@ ALTER TABLE `users`
 -- Tablo için AUTO_INCREMENT değeri `failed_jobs`
 --
 ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
--- Tablo için AUTO_INCREMENT değeri `files`
+-- Tablo için AUTO_INCREMENT değeri `get_data`
 --
-ALTER TABLE `files`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=36;
+ALTER TABLE `get_data`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=278;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `jobs`
+--
+ALTER TABLE `jobs`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=232;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `migrations`
 --
 ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `ocr_job_blocks`
+--
+ALTER TABLE `ocr_job_blocks`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=201;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `personal_access_tokens`
 --
 ALTER TABLE `personal_access_tokens`
   MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- Tablo için AUTO_INCREMENT değeri `rules`
+--
+ALTER TABLE `rules`
+  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- Tablo için AUTO_INCREMENT değeri `users`
